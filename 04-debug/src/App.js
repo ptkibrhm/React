8067cab274b7764b6c723df8226334e2e6f2f0af
ibrahim Petek <petevengapp@gmail.com>
2026-04-24 21:02:26 +0300

    debugger eklendi

---
+++ 04-debug/src/App.js
@@ -0,0 +1,42 @@
+import { useDebugValue, useState } from "react";
+
+function App() {
+  const [vize1, setVize1] = useState("");
+  const [vize2, setVize2] = useState("");
+  const [ortalama,setOrtalama]=useState("");
+
+  const ortalamaBul=()=>{
+    debugger
+    const sonuc = (Number(vize1)+Number(vize2))/2;
+    setOrtalama(sonuc)
+
+  }
+
+  return (
+    <div>
+      <div>
+        <input
+          type="number"
+          value={vize1}
+          onChange={(e) => setVize1(Number(e.target.value))}
+        ></input>
+      </div>
+
+      <div>
+        <input
+          type="number"
+          value={vize2}
+          onChange={(e) => setVize2(Number(e.target.value))}
+        ></input>
+      </div>
+
+      <div>
+        <button onClick={ortalamaBul}>Ortalama bul</button>
+      </div>
+
+     <p>Ortalama:{ortalama} </p>
+    </div>
+  );
+}
+
+export default App;
