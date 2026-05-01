5234df42580ef64e2f383a4194966051b854f10a
ibrahim Petek <petevengapp@gmail.com>
2026-05-01 21:42:54 +0300

    arayüz oluşturuldu useFormik kullanıldı RegisterForm

---
+++ 10-formik-yup/src/App.js
@@ -0,0 +1,12 @@
+import RegisterForm from "./components/RegisterForm";
+import "./App.css"
+
+function App() {
+  return (
+    <div className="App">
+      <RegisterForm />
+    </div>
+  );
+}
+
+export default App;
